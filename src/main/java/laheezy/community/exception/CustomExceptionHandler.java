5b7357e4996c6dd4edd55f9ff66ace4c99b57b9f
juhee park <jouhi3322@gmail.com>
2023-04-16 23:50:02 +0900

    feat : 인증 부분 예외처리부분 재수정

--- src/main/java/laheezy/community/exception/CustomExceptionHandler.java
+++ src/main/java/laheezy/community/exception/CustomExceptionHandler.java
@@ -4,6 +4,7 @@ package laheezy.community.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,21 +12,20 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @Slf4j
 @RestControllerAdvice
 public class CustomExceptionHandler {
-
-
-    @ExceptionHandler(CustomException.class)
-    public ResponseEntity<ErrorResult> checkLogin(CustomException e) {
+    @ExceptionHandler({CustomException.class, InternalAuthenticationServiceException.class})
+    public ResponseEntity<ErrorResult> catchError(CustomException e) {
         log.error("[전역 컨트롤러 예외처리에서 잡음] ex", e);
+
         ErrorCode errorCode = e.getErrorCode();
         ErrorResult errorResult = new ErrorResult(errorCode.getStatus().toString(), errorCode.getCode(), e.getMessage());
         //status 삭제 해야함
-        return new ResponseEntity(errorResult, e.getErrorCode().getStatus());
+        return new ResponseEntity<>(errorResult, e.getErrorCode().getStatus());
     }
 
-
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(Exception.class)
-    public ErrorResult checkLogin(Exception e) {
+    @ExceptionHandler
+    public ErrorResult catchErrorOther(Exception e) {
+        log.info("{}{}{}", e.getCause(), e.getMessage(), e.getClass());
         return new ErrorResult(HttpStatus.BAD_REQUEST.toString(), "에러 코드 수정 필요!!", e.getMessage());
     }
 
--- src/main/java/laheezy/community/service/CustomUserDetailService.java
+++ src/main/java/laheezy/community/service/CustomUserDetailService.java
@@ -1,6 +1,8 @@
 package laheezy.community.service;
 
 import laheezy.community.domain.Member;
+import laheezy.community.exception.CustomException;
+import laheezy.community.exception.ErrorCode;
 import laheezy.community.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
@@ -8,7 +10,6 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,26 +18,24 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class CustomUserDetailService implements UserDetailsService {
-
     private final MemberRepository memberRepository;
 
     @Override
     @Transactional
-    public UserDetails loadUserByUsername(String loginId) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String loginId) {
         return memberRepository.findOneWithAuthoritiesByLoginId(loginId)
-                .map(member -> createUser(loginId, member))
-                .orElseThrow(() -> new UsernameNotFoundException(loginId + " -> 데이터 베이스에서 찾을수 없습니다"));
+                .map(member -> createUser(member))
+                .orElseThrow(() -> new CustomException(ErrorCode.INVALID_MEMBER_PROFILE));
+        // .orElseThrow(() -> new UsernameNotFoundException(loginId + " -> 데이터 베이스에서 찾을수 없습니다"));
     }
 
-    private User createUser(String nickName, Member member) {
+    private User createUser(Member member) {
 
+        //활성화 되어있지 않다면(탈퇴한 멤버라면)
         if (!member.isActivated()) {
-            throw new RuntimeException(nickName + "-> 활성화 되어 있지 않습니다");
+            throw new CustomException(ErrorCode.INVALID_LOGIN_MEMBER);
         }
-        GrantedAuthority grantedAuthorities =new SimpleGrantedAuthority(member.getAuthority().toString());
-
-
-        //GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(member.getAuthorities().toString());
+        GrantedAuthority grantedAuthorities = new SimpleGrantedAuthority(member.getAuthority().toString());
 
         //유저 객체가 활성화 되어있다면 만들어서 리턴해준다.
         return new User(member.getLoginId(), member.getPassword(), List.of(grantedAuthorities));
