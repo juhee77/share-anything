950b1618992800957943d6d911adb186545d08d7
juhee77 <bora2233@naver.com>
2023-04-21 15:28:32 +0900

    feat : post 수정 API 생성
    - post의 text, title, board, 공개 여부 수정기능
    - post Api url 수정

--- src/main/java/laheezy/community/dto/post/PostModifyRequestForm.java
+++ src/main/java/laheezy/community/dto/post/PostModifyRequestForm.java
@@ -13,7 +13,6 @@ public class PostModifyRequestForm {
     private String title; //제목
     private String text; //내용
     private boolean isOpen; //공개 비공개
-    private LocalDateTime writeDate; //작성 날짜
     private String board;
 
 }
